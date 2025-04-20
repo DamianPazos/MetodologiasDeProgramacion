0755bcb07a6af5d0fdfb4c7e57eb9104c6d11a64
Damian Pazos <damian_ariel_pazos>
2025-04-20 16:48:31 -0300

    feat: Ejercicio N°5 - Practica N°1

--- Metodologias de Programacion/Practica N°1/Program.cs
+++ Metodologias de Programacion/Practica N°1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Practica_N_1.Clases;
+using Practica_N_1.Interfaces;
+
+namespace MetodologiasDeProgramacion;
+// Ejercicio N°5 - Practica N°1
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // codigo programa
+    }
+
+    public static void llenar(Coleccionable c)
+    {
+        Random random = new Random();
+        for (int contador = 1; contador < 21; contador++)
+        {
+            Comparable numero = new Numero(random.Next(1, 100));
+            c.agregar(numero);
+        }
+    }
+}
